fe694c4cfc4c15255d4b5b1186660c45b7cd3dd1
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2023-02-24 21:06:19 +0100

    Upgrade AGP to 7.4.1

--- app/src/main/java/com/example/android/foodbudg/MainActivity.java
+++ app/src/main/java/com/example/android/foodbudg/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.android.foodbudg;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +15,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.Serializable;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity /*implements AdapterView.OnItemSelectedListener */ {
--- app/src/main/java/com/example/android/foodbudg/OrderActivity.java
+++ app/src/main/java/com/example/android/foodbudg/OrderActivity.java
@@ -2,11 +2,12 @@ package com.example.android.foodbudg;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 //import static com.example.android.foodbudg.MainActivity.total;
 
